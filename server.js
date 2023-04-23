72bc839bc920f96fc2ae528038eb200319d8b798
Douglas Uretsky <douglasuretsky@gmail.com>
2023-04-23 10:58:44 -0400

    clean up server file

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 import express from "express";
-import { Server } from "socket.io";
 
-// const app = express();
+const app = express();
 
-// const users = [
-//   { id: 1, name: "Alice" },
-//   { id: 2, name: "Bob" },
-//   { id: 3, name: "Charlie" },
-// ];
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
 
-// app.get("/api/v1", (req, res) => {
-//   res.json(users);
-// });
-
-// app.listen(3000, () => {
-//   console.log("Express app listening on port 3000");
-// });
-
-const io = new Server({
-  /* options */
+app.get("/api/v1", (req, res) => {
+  res.json(users);
 });
 
-io.on("connection", (socket) => {
-  console.log(socket.id); // ojIckSD2jqNzOqIrAGzL
+app.listen(3000, () => {
+  console.log("Express app listening on port 3000");
 });
-
-io.listen(3000);
