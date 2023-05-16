2d83fe82567e4c51d4c868db8beb95de2a991467
Douglas Uretsky <douglasuretsky@gmail.com>
2023-05-16 10:30:15 -0400

    set dev server

--- socket.js
+++ socket.js
@@ -9,7 +9,7 @@ app.use(cors()); // Allow requests from any origin
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5173",
+    origin: "http://localhost:4000",
     methods: ["GET", "POST"], // Allow requests from any origin
   },
 });
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "localhost",
+    port: 4000,
     proxy: {
       // proxy /api requests to http://localhost:3000
       "/api/v1": "http://localhost:3000",
